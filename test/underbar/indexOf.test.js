52aa9c3771fb6bda43e7153ef7174e7a5ef0ac2b
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 16:52:27 -0700

    implement functionality and tests for _.indexOf

--- test/underbar/indexOf.test.js
+++ test/underbar/indexOf.test.js
@@ -2,23 +2,33 @@ const _ = require('../../underbar');
 
 describe('indexOf()', () => {
   it('returns the index of a value at the beginning of an array', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c', 'd', 'e'];
+    const result = 0;
+    expect(_.indexOf(arr, 'a')).toEqual(result);
   });
 
   it('returns the index of a value at the end of an array', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c', 'd', 'e'];
+    const result = arr.length - 1;
+    expect(_.indexOf(arr, 'e')).toEqual(result);
   });
 
   it('returns -1 for a missing value', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c', 'd', 'e'];
+    const result = -1;
+    expect(_.indexOf(arr, 'q')).toEqual(result);
   });
 
   it('returns the first matching index when multiple matches in array', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c', 'c', 'b', 'a'];
+    const result = 2;
+    expect(_.indexOf(arr, 'c')).toEqual(result);
   });
 
   it('starts searching at the given offset', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c', 'c', 'b', 'a'];
+    const result = 4;
+    expect(_.indexOf(arr, 'b', 2)).toEqual(result);
   });
 
 });

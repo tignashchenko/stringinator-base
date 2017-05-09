e0dfc18d8c52cbfa87d967ccf5c1d1e5e6aea86b
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 21:25:55 -0700

    implement functionality and tests for _.some

--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -2,11 +2,12 @@ const _ = require('../../underbar');
 
 describe('some()', () => {
   it('returns true if any number is odd', () => {
-    // Your code goes here
+    const numbers = [1, 2, 3, 4, 5];
+    expect(_.some(numbers, element => element % 2 !== 0)).toEqual(true);
   });
 
   it('returns false if no number is odd', () => {
-    // Your code goes here
+    const evenNumbers = [2, 4, 6, 8, 10];
+    expect(_.some(evenNumbers, element => element % 2 !== 0)).toEqual(false);
   });
-
 });

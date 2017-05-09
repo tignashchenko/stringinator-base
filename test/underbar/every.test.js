db7bb69fc2156e2e8bb890e22ed2d34a8493a836
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 21:18:56 -0700

    implement functionality and tests for _.every

--- test/underbar/every.test.js
+++ test/underbar/every.test.js
@@ -3,16 +3,18 @@ const _ = require('../../underbar');
 describe('every()', () => {
   describe('processing an array of numbers', () => {
     it('will return true if no callback is supplied', () => {
-      // Your code goes here
+      const numbers = [1, 2, 3, 4, 5];
+      expect(_.every(numbers)).toEqual(true);
     });
 
     it('returns true if all numbers in an array are odd and we test for odd numbers', () => {
-      // Your code goes here
+      const oddNumbers = [1, 3, 5, 7, 9];
+      expect(_.every(oddNumbers, element => element % 2 !== 0)).toEqual(true);
     });
 
     it('returns false if not all numbers in an array are odd and we test for odd numbers', () => {
-      // Your code goes here
+      const numbers = [1, 2, 3, 4, 5];
+      expect(_.every(numbers, element => element % 2 !== 0)).toEqual(false);
     });
-
   });
 });

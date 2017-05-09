590b30a56a0553e3788ff54a7c8ae0c24a0bc2e9
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 20:27:28 -0700

    implement functionality and tests for _.pluck

--- test/underbar/pluck.test.js
+++ test/underbar/pluck.test.js
@@ -2,7 +2,13 @@ const _ = require('../../underbar');
 
 describe('pluck()', () => {
   it('returns an array of just ages, given an array of people objects', () => {
-    // Your code goes here
+    const family = [
+      {name: 'Taras', age: 26},
+      {name: 'Volodymyr', age: 57},
+      {name: 'Olga', age: 57},
+      {name: 'Bogdan', age: 29}
+    ];
+    const result = [26, 57, 57, 29];
+    expect(_.pluck(family, 'age')).toEqual(result);
   });
-
 });

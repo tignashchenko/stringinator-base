7b176c468b56857d390c093492386f0daa8b7584
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 21:13:32 -0700

    implement functionality and tests for _.contains

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -2,19 +2,32 @@ const _ = require('../../underbar');
 
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
-    // Your code goes here
+    const names = ['Olga', 'Volodymyr', 'Taras', 'Bogdan'];
+    expect(_.contains(names, 'Volodymyr')).toEqual(true);
   });
 
   it('returns false if an array does not contain the target', () => {
-    // Your code goes here
+    const names = ['Olga', 'Volodymyr', 'Taras', 'Bogdan'];
+    expect(_.contains(names, 'Tim')).toEqual(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
-    // Your code goes here
+    const superHeroes = {
+      hero1: 'Batman',
+      hero2: 'Spider Man',
+      hero3: 'Daredevil',
+      hero4: 'Iron Man'
+    };
+    expect(_.contains(superHeroes, 'Daredevil')).toEqual(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
-    // Your code goes here
+    const superHeroes = {
+      hero1: 'Batman',
+      hero2: 'Spider Man',
+      hero3: 'Daredevil',
+      hero4: 'Iron Man'
+    };
+    expect(_.contains(superHeroes, 'Hulk')).toEqual(false);
   });
-
 });

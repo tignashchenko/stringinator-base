dc175e46b195ccd74e51643b5f5cd686c548e7a1
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 22:00:14 -0700

    implement functionality and tests for _.uniq

--- test/underbar/uniq.test.js
+++ test/underbar/uniq.test.js
@@ -2,6 +2,8 @@ const _ = require('../../underbar');
 
 describe('uniq()', () => {
   it('de-dups a list of numbers', () => {
-    // Your code goes here
+    const numbers = [1, 3, 5, 2, 3, 1, 7, 3, 1, 5, 5];
+    const result = [1, 3, 5, 2, 7];
+    expect(_.uniq(numbers)).toEqual(result);
   });
 });

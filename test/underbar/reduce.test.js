a68eecaad56f5ebf1598fc9c79998a9dda0071ba
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 20:34:51 -0700

    implement functionality and tests for _.reduce

--- test/underbar/reduce.test.js
+++ test/underbar/reduce.test.js
@@ -2,11 +2,14 @@ const _ = require('../../underbar');
 
 describe('reduce()', () => {
   it('reduces an array of numbers to a sum, *with* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    const numbers = [1, 2, 3, 4, 5];
+    const result = 15;
+    expect(_.reduce(numbers, (acc, curr) => acc + curr), 0).toEqual(result);
   });
 
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    const numbers = [1, 2, 3, 4, 5];
+    const result = 15;
+    expect(_.reduce(numbers, (acc, curr) => acc + curr)).toEqual(result);
   });
-
 });

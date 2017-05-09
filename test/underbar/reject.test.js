0d79469d0bf73bfd0aa3634629ec4c06f5f3bf02
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 21:55:18 -0700

    implement functionality and tests for _.reject

--- test/underbar/reject.test.js
+++ test/underbar/reject.test.js
@@ -2,10 +2,19 @@ const _ = require('../../underbar');
 
 describe('reject()', () => {
   it('rejects odd numbers from an array', () => {
-    // Your code goes here
+    const number = [1, 2, 3, 4, 5];
+    const result = [2, 4];
+    expect(_.reject(number, element => element % 2 !== 0)).toEqual(result);
   });
 
   it('rejects null values from an object', () => {
-    // Your code goes here
+    const obj = {
+      'name': 'Taras',
+      'HR diploma': null,
+      'age': 26,
+      'spouse': null
+    };
+    const result = ['Taras', 26];
+    expect(_.reject(obj, value => value === null)).toEqual(result);
   });
 });

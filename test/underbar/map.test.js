57dfb9e99e6303c06ae2f8e50ca3a58a6d740bc4
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-08 20:22:09 -0700

    implement functionality and tests for _.map

--- test/underbar/map.test.js
+++ test/underbar/map.test.js
@@ -2,6 +2,8 @@ const _ = require('../../underbar');
 
 describe('map()', () => {
   it('maps every numbers in an array of numbers to their square', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4, 5];
+    const result = [1, 4, 9, 16, 25];
+    expect(_.map(arr, element => element * element)).toEqual(result);
   });
 });
